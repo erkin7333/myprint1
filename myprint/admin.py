12f8ef2f8c67f7f543283ae0d01355da73e6e86c
erkin7333 <erkinnajimov90@gmail.com>
2022-10-22 13:59:31 +0500

    Admin panel uchun qilingan ishlar

--- myprint/admin.py
+++ myprint/admin.py
@@ -3,6 +3,7 @@ from django_admin_listfilter_dropdown.filters import (
     DropdownFilter, ChoiceDropdownFilter, RelatedDropdownFilter
 )
 from .models import *
+from django.utils.html import format_html
 
 
 # Register your models here.
@@ -14,7 +15,7 @@ class ProductAdmin(admin.ModelAdmin):
     list_display_links = [
         'category', 'name_uz', 'name_ru'
     ]
-
+    list_per_page = 2
     class Meta:
         model = Product
 
@@ -47,7 +48,7 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display_links = [
         'parent', 'name_uz', 'name_ru',
     ]
-
+    list_per_page = 2
     class Meta:
         model = Category
 
@@ -60,7 +61,7 @@ class SettingsAdmin(admin.ModelAdmin):
         'key',
         'value'
     ]
-
+    list_per_page = 2
     class Meta:
         model = Settings
 
@@ -75,7 +76,7 @@ class TypeServiceAdmin(admin.ModelAdmin):
     list_display_links = [
         'name_uz', 'name_ru'
     ]
-
+    list_per_page = 2
     class Meta:
         model = TypeService
 
@@ -90,7 +91,7 @@ class ImageAdmin(admin.ModelAdmin):
     list_display_links = [
         'type_sevice'
     ]
-
+    list_per_page = 2
     class Meta:
         model = Image
 
@@ -106,6 +107,7 @@ class MenuServiceAdmin(admin.ModelAdmin):
     list_display_links = [
         'name_uz', 'name_ru'
     ]
+    list_per_page = 2
 
     class Meta:
         model = MenuService
@@ -121,7 +123,7 @@ class AboutAdmin(admin.ModelAdmin):
     list_display_links = [
         "description_uz", "description_ru",
     ]
-
+    list_per_page = 2
     class Meta:
         model = About
 
@@ -136,6 +138,7 @@ class AboutImageAdmin(admin.ModelAdmin):
     list_display_links = [
         "name_uz", "name_ru"
     ]
+    list_per_page = 2
 
     class Meta:
         model = AboutImage
@@ -151,7 +154,7 @@ class TypeAdmin(admin.ModelAdmin):
     list_display_links = [
         'name_uz', 'name_ru'
     ]
-
+    list_per_page = 2
     class Meta:
         model = Type
 
@@ -160,20 +163,25 @@ admin.site.register(Type, TypeAdmin)
 
 
 class TypeAdminService(admin.ModelAdmin):
+    readonly_fields = ('photo_tag1', 'photo_tag2', 'photo_tag3')
     list_display = [
         'id', 'size', 'type_paper', 'one_site_print',
         'double_site_print', 'shiroki_size', 'shiroki_name', 'shiroki_price',
         'tekstil_size', 'tekstil_price', 'lazer_size', 'lazer_price',
-        'image1', 'image2', 'image3',
+        'photo_tag1', 'photo_tag2', 'photo_tag3'
     ]
     list_display_links = [
         'size', 'type_paper', 'one_site_print',
-        'double_site_print', 'image1'
+        'double_site_print'
     ]
-    # list_filter = (
-    #     ("type", admin.RelatedOnlyFieldListFilter)
-    # )
-
+    list_filter = ('type', )
+    list_per_page = 1
+    def photo_tag1(self, obj):
+        return format_html(f'<img src="{obj.image1.url}" style="height:100px; width:100px; border-radius: 50%">')
+    def photo_tag2(self, obj):
+        return format_html(f'<img src="{obj.image2.url}" style="height:100px; width:100px; border-radius: 50%">')
+    def photo_tag3(self, obj):
+        return format_html(f'<img src="{obj.image3.url}" style="height:100px; width:100px; border-radius: 50%">')
     class Meta:
         model = Type_Services
 
